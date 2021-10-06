10195581fd7e5bff6894f285045d32a5579d5673
ColinOConnor24 <818704@mystma.org>
2021-10-06 09:10:16 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    letter = 0
+    acount = 0
+    for i in range(1, len(w) + 1):
+        if w[letter] == "a":
+            acount += 1
+        letter += 1
+    return acount
